856a1fd4c7868116ff8b5525e7caf3ba76b20843
Rajubhuiyan <rajubhuiyaann@gmail.com>
2023-05-27 19:02:51 +0600

    navbar added

--- src/Pages/Homepage/Components/Header.js
+++ src/Pages/Homepage/Components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import HeaderBg from '../../../Utilities/Images/Homepage/header-background.png';
 import headerRightImg from '../../../Utilities/Images/Homepage/header-right-image.png';
 import { Avatar, Card, IconButton, InputAdornment, Rating, TextField, Typography, styled } from '@mui/material';
 import { ArrowForward, Search } from '@mui/icons-material';
@@ -70,196 +69,183 @@ const headerCardData = [
 
 const Header = () => {
     return (
-        <div>
+        <Container fullWidth="md" >
             <Box sx={{
-                backgroundImage: `url(${HeaderBg})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                // height: '100vh',
-                width: '100%',
-                backgroundColor: 'white',
-                backgroundPosition: 'center',
+                py: { xs: 12, md: 9 },
+                position: 'relative',
             }}>
+                <Box>
+                    <Typography color="#685F78" >
+                        The Leader in Online Learning
+                    </Typography>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6} md={6}>
+                            <Box>
 
-                <Container fullWidth="md" >
-                    <Box sx={{
-                        py: {xs:12,md:9},
-                        position: 'relative',
-                    }}>
-                        <Box>
-                            <Typography color="#685F78" >
-                                The Leader in Online Learning
-                            </Typography>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12} sm={6} md={6}>
-                                    <Box>
+                                <Typography sx={{
+                                    color: '#002058',
+                                    fontSize: { xs: '1.7rem', sm: '2.6rem', lg: '3.5rem' },
+                                    fontWeight: 'bold',
+                                    lineHeight: '1.2',
+                                }}>
+                                    Engaging &
+                                    Accessible Online
+                                    Courses For All
+                                </Typography>
+                            </Box>
 
-                                        <Typography sx={{
-                                            color: '#002058',
-                                            fontSize: {xs:'1.7rem',sm:'2.6rem',lg:'3.5rem'},
-                                            fontWeight: 'bold',
-                                            lineHeight: '1.2',
-                                        }}>
-                                            Engaging &
-                                            Accessible Online
-                                            Courses For All
-                                        </Typography>
-                                    </Box>
-
-                                    {/* search bar */}
-                                    <Box sx={{
-                                        mt: 4
-                                    }}>
-                                        <SearchTextField
-                                            InputProps={{
-                                                startAdornment: (
-                                                    <InputAdornment position='start' >
-                                                        <Search />
-                                                    </InputAdornment>
-                                                ),
-                                                endAdornment: (
-                                                    <InputAdornment position='end' >
-                                                        <IconButton sx={{
-                                                            color: 'white',
-                                                            backgroundColor: '#FF6575',
-                                                            '&:hover': {
-                                                                backgroundColor: '#FF6575',
-                                                            },
-                                                        }}>
-                                                            <ArrowForward />
-                                                        </IconButton>
-                                                    </InputAdornment>
-                                                )
-                                            }}
-
-                                            fullWidth placeholder='Search School, Online eductional centers, etc' />
-                                    </Box>
-
-
-                                    {/* review */}
-
-                                    <Box sx={{
-                                        mt: 4
-                                    }}>
-                                        <Typography gutterBottom sx={{
-                                            color: '#685F78',
-                                            fontSize: '1rem',
-                                            fontWeight: 'bold',
-                                            lineHeight: '1.2',
-                                        }}>
-                                            Trusted by over 15K Users
-                                        </Typography>
-                                        <Typography sx={{
-                                            color: '#685F78',
-                                            fontSize: '1rem',
-                                            fontWeight: 'bold',
-                                            lineHeight: '1.2',
-                                        }}>
-                                            worldwide since 2022
-                                        </Typography>
-                                    </Box>
-
-                                    {/* rating */}
-                                    <Box sx={{
-                                        mt: 1,
-                                        display: 'flex',
-                                        gap: 3,
-                                        alignItems: 'center'
-                                    }}>
-                                        <Typography sx={{
-                                            color: '#002058',
-                                            fontSize: '2rem',
-                                            fontWeight: '600'
-                                        }}>
-                                            290+
-                                        </Typography>
-                                        <Typography sx={{
-                                            color: '#002058',
-                                            display: 'flex',
-                                            gap: 1,
-                                            alignItems: 'center',
-                                            fontSize: '2rem',
-                                            fontWeight: '600'
-                                        }}>
-                                            4.5
-                                            <Rating size='small' name="read-only" value={4.5} readOnly />
-                                        </Typography>
-
-                                    </Box>
-
-                                </Grid>
-                                <Grid item xs={12} sm={6} md={6}>
-                                    <Box sx={{
-                                        height: {xs:250,md:450},
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                    }}>
-                                        <img style={{
-                                            width: 'auto',
-                                            height: '100%',
-                                        }} src={headerRightImg} alt="" />
-                                    </Box>
-                                </Grid>
-                            </Grid>
-                        </Box>
-
-
-                        <Box sx={{
-                            position: 'absolute',
-                            width: '100%',
-                            bottom: {xs:'-60px',sm:'-22px',md:'-33px'},
-                            display: 'flex',
-                            justifyContent: 'center'
-                        }}>
+                            {/* search bar */}
                             <Box sx={{
+                                mt: 4
+                            }}>
+                                <SearchTextField
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position='start' >
+                                                <Search />
+                                            </InputAdornment>
+                                        ),
+                                        endAdornment: (
+                                            <InputAdornment position='end' >
+                                                <IconButton sx={{
+                                                    color: 'white',
+                                                    backgroundColor: '#FF6575',
+                                                    '&:hover': {
+                                                        backgroundColor: '#FF6575',
+                                                    },
+                                                }}>
+                                                    <ArrowForward />
+                                                </IconButton>
+                                            </InputAdornment>
+                                        )
+                                    }}
+
+                                    fullWidth placeholder='Search School, Online eductional centers, etc' />
+                            </Box>
+
+
+                            {/* review */}
+
+                            <Box sx={{
+                                mt: 4
+                            }}>
+                                <Typography gutterBottom sx={{
+                                    color: '#685F78',
+                                    fontSize: '1rem',
+                                    fontWeight: 'bold',
+                                    lineHeight: '1.2',
+                                }}>
+                                    Trusted by over 15K Users
+                                </Typography>
+                                <Typography sx={{
+                                    color: '#685F78',
+                                    fontSize: '1rem',
+                                    fontWeight: 'bold',
+                                    lineHeight: '1.2',
+                                }}>
+                                    worldwide since 2022
+                                </Typography>
+                            </Box>
+
+                            {/* rating */}
+                            <Box sx={{
+                                mt: 1,
+                                display: 'flex',
+                                gap: 3,
+                                alignItems: 'center'
+                            }}>
+                                <Typography sx={{
+                                    color: '#002058',
+                                    fontSize: '2rem',
+                                    fontWeight: '600'
+                                }}>
+                                    290+
+                                </Typography>
+                                <Typography sx={{
+                                    color: '#002058',
+                                    display: 'flex',
+                                    gap: 1,
+                                    alignItems: 'center',
+                                    fontSize: '2rem',
+                                    fontWeight: '600'
+                                }}>
+                                    4.5
+                                    <Rating size='small' name="read-only" value={4.5} readOnly />
+                                </Typography>
+
+                            </Box>
+
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={6}>
+                            <Box sx={{
+                                height: { xs: 250, md: 450 },
                                 display: 'flex',
                                 justifyContent: 'center',
-                                width: {xs:'100%',lg:'80%'}
                             }}>
-                                <Grid container spacing={{xs:1,md:2}}>
-                                    {
-                                        headerCardData.map((data) => (
-                                            <Grid key={data.id} item xs={6} sm={3} md={3}>
-                                                <Card sx={{
-                                                    p: {xs:1,md:2},
-                                                    borderRadius: 2,
-                                                    display: 'flex',
-                                                    alignItems: 'center',
-                                                    gap: 1,
-                                                    boxShadow: 'none',
-                                                }}>
-                                                    <Avatar
-                                                        src={data.img}
-                                                        alt={data.title}
-                                                    />
-                                                    <Box>
-                                                        <Typography gutterBottom sx={{
-                                                            color: '#002058',
-                                                            fontWeight: '600',
-                                                            fontSize: {xs:'1rem',md:'1.2rem'},
-                                                            lineHeight: '1'
-                                                        }} >
-                                                            {data.value}
-                                                        </Typography>
-                                                        <Typography sx={{
-                                                            fontSize: {xs:'0.6rem',md:'0.8rem'}
-                                                        }}>
-                                                            {data.title}
-                                                        </Typography>
-                                                    </Box>
-                                                </Card>
-                                            </Grid>
-                                        ))
-                                    }
-                                </Grid>
+                                <img style={{
+                                    width: 'auto',
+                                    height: '100%',
+                                }} src={headerRightImg} alt="" />
                             </Box>
-                        </Box>
+                        </Grid>
+                    </Grid>
+                </Box>
 
+
+                <Box sx={{
+                    position: 'absolute',
+                    width: '100%',
+                    bottom: { xs: '-60px', sm: '-22px', md: '-33px' },
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: { xs: '100%', lg: '80%' }
+                    }}>
+                        <Grid container spacing={{ xs: 1, md: 2 }}>
+                            {
+                                headerCardData.map((data) => (
+                                    <Grid key={data.id} item xs={6} sm={3} md={3}>
+                                        <Card sx={{
+                                            p: { xs: 1, md: 2 },
+                                            borderRadius: 2,
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            gap: 1,
+                                            boxShadow: 'none',
+                                        }}>
+                                            <Avatar
+                                                src={data.img}
+                                                alt={data.title}
+                                            />
+                                            <Box>
+                                                <Typography gutterBottom sx={{
+                                                    color: '#002058',
+                                                    fontWeight: '600',
+                                                    fontSize: { xs: '1rem', md: '1.2rem' },
+                                                    lineHeight: '1'
+                                                }} >
+                                                    {data.value}
+                                                </Typography>
+                                                <Typography sx={{
+                                                    fontSize: { xs: '0.6rem', md: '0.8rem' }
+                                                }}>
+                                                    {data.title}
+                                                </Typography>
+                                            </Box>
+                                        </Card>
+                                    </Grid>
+                                ))
+                            }
+                        </Grid>
                     </Box>
-                </Container>
+                </Box>
 
-            </Box>
-        </div>
+            </Box> 
+        </Container>
+
     );
 };
 
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from './Components/Header';
 import { Box } from '@mui/material';
+import Navbar from './Components/Navbar';
+import HeaderBg from '../../Utilities/Images/Homepage/header-background.png';
 
 const Homepage = () => {
     return (
-        <Box sx={{maxWidth:'100vw', }}> 
-            <Header/>
+        <Box sx={{ maxWidth: '100vw', }}>
+            <Box sx={{
+                backgroundImage: `url(${HeaderBg})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                // height: '100vh',
+                width: '100%',
+                backgroundColor: 'white',
+                backgroundPosition: 'center',
+                mb:{xs:'100px',md:'50px'}
+            }}>
+                <Navbar />
+                <Header />
+            </Box>
         </Box>
     );
 };
