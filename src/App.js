0df5c7af7bfbe425c17642ecb70f67d4b1f61d07
Rajubhuiyan <rajubhuiyaann@gmail.com>
2023-09-12 16:50:38 +0600

    login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
-import { RouterProvider } from 'react-router-dom'; 
+import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Router';
 import 'swiper/css'; // swiper js bundle import
 import 'swiper/css/pagination'; // swiper pagination style import
 import "swiper/css/free-mode";
-
+import { initializeApp } from 'firebase/app';
+import { useSelector } from 'react-redux';
 
 function App() {
 
-  
+  const firebaseConfig = {
+    apiKey: "AIzaSyBzU8fzn2QKf5PgMErFbQ1h75mVLQnucDU",
+    authDomain: "mastery-hub.firebaseapp.com",
+    projectId: "mastery-hub",
+    storageBucket: "mastery-hub.appspot.com",
+    messagingSenderId: "505247571949",
+    appId: "1:505247571949:web:d00650fdb11dcbf86451e4"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+
+
+  const loggedUser = useSelector(state => state.loggedUser.loggedUserData)
+
+
+  console.log(loggedUser,'loggedUser')
 
   return (
-     <RouterProvider router={router}>
+    <RouterProvider router={router}>
 
-     </RouterProvider>
+    </RouterProvider>
   );
 }
 
